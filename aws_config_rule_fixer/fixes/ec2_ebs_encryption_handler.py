c12b4ecd30d7bb5429560358d6556d143a9bc646
James Hodgkinson <james@terminaloutcomes.com>
2024-07-07 15:13:03 +1000

    working on things

---
+++ aws_config_rule_fixer/fixes/ec2_ebs_encryption_handler.py
@@ -0,0 +1,15 @@
+# import sys
+from typing import Optional
+
+# import boto3
+import click  # type: ignore
+from loguru import logger  # type: ignore
+
+# from aws_config_rule_fixer import get_all_regions
+
+
+@click.command()
+def main(region: Optional[str] = None) -> None:
+    """ " does things"""
+
+    logger.info("Using region: {}", region)
